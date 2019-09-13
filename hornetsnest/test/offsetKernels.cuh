6e4c8110bdc12eb8008711a61fd0c9b64f5e51fe
Oded Green <ogreen@nvidia.com>
2019-09-13 17:05:10 -0400

    parsing rows and now ready to look for words

--- hornetsnest/test/offsetKernels.cuh
+++ hornetsnest/test/offsetKernels.cuh
@@ -23,14 +23,87 @@ struct findAndCountArray {
 	string_t myChar;
 	int* count;
 	string_t *dataString;
-	
+
 	OPERATOR(int i){
     	if(dataString[i] == myChar){
     		atomicAdd(count,1);
     		// printf("*");
     	}
 	}
-
 };
+
+struct findAndStoreLineEnds {
+	string_t myChar;
+	int* count;
+	string_t *dataString;
+	soffset_t *positions;
+
+	OPERATOR(int i){
+    	if(dataString[i] == myChar){
+    		soffset_t myPos = atomicAdd(count,1);
+    		positions[myPos] = i;
+    		// printf("*");
+    	}
+	}
+};
+
+
+struct countWords {
+	string_t wordSplitter;
+	int* count;
+	string_t *dataString;
+	soffset_t *rowOffsets;
+	soffset_t lineCounts;
+	// soffset_t *positions;
+
+	OPERATOR(int i){
+
+		soffset_t startPos = rowOffsets[i];
+		soffset_t len 		= rowOffsets[i+1]-rowOffsets[i];
+		if(len>0)
+			len--; //removing '\n' from string
+
+		// if(i>=(lineCounts-10))
+		if(i<=(10) || i>=(lineCounts-10))
+			printf("%d %d %d\n", i,rowOffsets[i+1],rowOffsets[i]);
+		// return;
+	    for (int i=0; i<len; i++) {
+	    	if(i==0 && dataString[startPos+i] == wordSplitter){
+				// atomicAdd(count,1);    		// printf("*");
+	    	}else if(i>0){
+		    	if(dataString[startPos+i] == wordSplitter && dataString[startPos+i-1]!= wordSplitter){
+		    		atomicAdd(count,1);    		// printf("*");
+		    	}
+	    	}
+	    }
+	}
+};
+
+
+// struct countWords {
+// 	string_t wordSplitter;
+// 	string_t lineSplitter;
+// 	int* count;
+// 	string_t *dataString;
+// 	int length;
+
+// 	OPERATOR(int i){
+//     	// if(dataString[i] == wordSplitter && (i+1)<(length) && dataString[i+1]!= wordSplitter){
+//     	// if(dataString[i] == wordSplitter && (i-1)>0 && dataString[i-1]!= wordSplitter && dataString[i-1]!= lineSplitter){
+ 
+// 	   	if(dataString[i] == lineSplitter){
+// 	    		atomicAdd(count,1);
+// 	    		// printf("*");
+// 	    }
+
+//     	if(dataString[i] == wordSplitter && (i+1)<length && dataString[i-1]!= wordSplitter && dataString[i-1]!= lineSplitter){
+//     		atomicAdd(count,1);    		// printf("*");
+//     	}
+ 
+// 	}
+// };
+
+
+
 
 } // hornets_nest namespace
--- hornetsnest/test/offsetTest.cu
+++ hornetsnest/test/offsetTest.cu
@@ -40,11 +40,10 @@ int exec(int argc, char* argv[]) {
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
 
-    soffset_t wordCount   = 3;
-    soffset_t letterCount = 11; 
-
-    string_t letters[letterCount]       = {'m','y','f','i','r','s','t','t','e','s','t'};
-    soffset_t wordOffsets[wordCount+1]  = {0,2,7,11};
+    // soffset_t wordCount   = 3;
+    // soffset_t letterCount = 11; 
+    // string_t letters[letterCount]       = {'m','y','f','i','r','s','t','t','e','s','t'};
+    // soffset_t wordOffsets[wordCount+1]  = {0,2,7,11};
 
     // HornetInit hornet_init(wordCount,letterCount,wordOffsets,letters);
     // HornetStaticGraph hgraph(hornet_init);
@@ -74,21 +73,68 @@ int exec(int argc, char* argv[]) {
         size_t readSize = fread(h_fileInfo, 1, fsize, f);
         fclose(f);
 
-        h_fileInfo[fsize] = 0;    
+        h_fileInfo[fsize++] = '\n';    
 
         string_t* d_fileInfo;
         cudaMalloc((string_t**)&d_fileInfo, sizeof(string_t)*fsize);
         cudaMemcpy(d_fileInfo,h_fileInfo, sizeof(string_t)*fsize, cudaMemcpyHostToDevice);
         cudaMemset(dCounter,0, sizeof(int));
 
-        CHECK_ERROR("Failing before allocation")
         string_t myChar='\n';
-        // forAll(fsize, hornets_nest::findAndCountArray {myChar, dCounter, d_fileInfo });
+        forAll(fsize, hornets_nest::findAndCountArray {myChar, dCounter, d_fileInfo });
 
         int hlineCounts = 0;
         cudaMemcpy(&hlineCounts,dCounter,sizeof(int), cudaMemcpyDeviceToHost);
-
         printf("Line Count : %d \n",hlineCounts);
+
+        soffset_t *rowStarts,*rowStartsSorted;//,*rowOffsets;
+
+        gpu::allocate(rowStartsSorted,hlineCounts+1);
+        gpu::allocate(rowStarts,hlineCounts+1);
+        // gpu::allocate(rowOffsets,hlineCounts+1);
+        cudaMemset(dCounter,0, sizeof(int));
+        cudaMemset(rowStarts,0, sizeof(int));
+
+        forAll(fsize, hornets_nest::findAndStoreLineEnds {'\n', dCounter, d_fileInfo,rowStarts+1});
+
+        cudaMemcpy(&hlineCounts,dCounter,sizeof(int), cudaMemcpyDeviceToHost);
+        printf("Line Count : %d \n",hlineCounts);
+
+
+        // Sorting the starting positions
+        {
+            void     *d_temp_storage = NULL;
+            size_t   temp_storage_bytes = 0;
+            cub::DeviceRadixSort::SortKeys(d_temp_storage, temp_storage_bytes, rowStarts, rowStartsSorted, hlineCounts+1);
+            cudaMalloc(&d_temp_storage, temp_storage_bytes);
+            cub::DeviceRadixSort::SortKeys(d_temp_storage, temp_storage_bytes, rowStarts, rowStartsSorted, hlineCounts+1);
+            cudaFree(d_temp_storage);
+        }
+
+            // hlineCounts--; // Removing the last empty row that got add because the last character is '\n'
+
+        // // Creating offset array based on sorted data
+        // {
+        //     void     *d_temp_storage = NULL;
+        //     size_t   temp_storage_bytes = 0;
+        //     cub::DeviceScan::InclusiveSum(d_temp_storage, temp_storage_bytes, rowStartsSorted, rowOffsets+1, hlineCounts);
+        //     cudaMalloc(&d_temp_storage, temp_storage_bytes);
+        //     cub::DeviceScan::InclusiveSum(d_temp_storage, temp_storage_bytes, rowStartsSorted, rowOffsets+1, hlineCounts);            
+        //     cudaFree(d_temp_storage);
+        // }
+
+
+        cudaMemset(dCounter,0, sizeof(int));
+        forAll(hlineCounts, hornets_nest::countWords {' ', dCounter, d_fileInfo,rowStartsSorted,hlineCounts });
+        int hWordCounts = 0;
+        cudaMemcpy(&hWordCounts,dCounter,sizeof(int), cudaMemcpyDeviceToHost);
+        printf("Word Count : %d \n",hWordCounts);
+
+
+        gpu::free(rowStarts);
+        gpu::free(rowStartsSorted);
+        // gpu::free(rowOffsets);
+
 
         free(h_fileInfo);
         cudaFree(d_fileInfo);
@@ -111,14 +157,14 @@ int exec(int argc, char* argv[]) {
 
 int main(int argc, char* argv[]) {
     int ret = 0;
-    hornets_nest::gpu::initializeRMMPoolAllocation();//update initPoolSize if you know your memory requirement and memory availability in your system, if initial pool size is set to 0 (default value), RMM currently assigns half the device memory.
+    // hornets_nest::gpu::initializeRMMPoolAllocation();//update initPoolSize if you know your memory requirement and memory availability in your system, if initial pool size is set to 0 (default value), RMM currently assigns half the device memory.
     {//scoping technique to make sure that hornets_nest::gpu::finalizeRMMPoolAllocation is called after freeing all RMM allocations.
 
     //ret = exec<hornets_nest::HornetDynamicGraph, hornets_nest::BfsTopDown2Dynamic>(argc, argv);
     ret = exec<hornets_nest::HornetStaticGraph>(argc, argv);
 
     }//scoping technique to make sure that hornets_nest::gpu::finalizeRMMPoolAllocation is called after freeing all RMM allocations.
-    hornets_nest::gpu::finalizeRMMPoolAllocation();
+    // hornets_nest::gpu::finalizeRMMPoolAllocation();
 
     return ret;
 }
